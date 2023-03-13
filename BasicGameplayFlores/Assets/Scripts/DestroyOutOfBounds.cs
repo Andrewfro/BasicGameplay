4c6f8899538ade7274c4ef61cef30f86c32f2251
Andrewfro <743961@student.dusd.net>
2023-03-13 09:51:19 -0700

    Worked on project

--- BasicGameplayFlores/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplayFlores/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,5 +19,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
+       
